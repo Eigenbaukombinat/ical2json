515cbf6508e58e9127f15eed790744bddb18601c
nilo <nilo@team-tfm.com>
2019-12-02 19:38:32 +0100

    add timezone, fix requirements

--- ical2json.py
+++ ical2json.py
@@ -2,23 +2,29 @@ from flask import Flask, jsonify
 from icalevents import icalevents
 import datetime
 import gocept.cache.method
+import pytz
+
+BERLIN = pytz.timezone("Europe/Berlin")
 
 
 @gocept.cache.method.Memoize(60)
 def get_events():
-    start = datetime.datetime.now() - datetime.timedelta(hours=10)
-    end = datetime.datetime.now() + datetime.timedelta(days=100)
+    start = datetime.datetime.utcnow() - datetime.timedelta(hours=10)
+    end = datetime.datetime.utcnow() + datetime.timedelta(days=100)
     events = icalevents.events('https://eigenbaukombinat.de/all.ics', 
-                               start=start, end=end)
+                               start=BERLIN.localize(start), 
+                               end=BERLIN.localize(end))
     next_events = []
     for event in events:
+        startdt = event.start.astimezone(BERLIN)
+        enddt = event.end.astimezone(BERLIN)
         next_events.append(dict(
             summary=event.summary,
-            sortdate=event.start.strftime('%Y%m%d%H%M'),
-            startdate=event.start.strftime('%d.%m.%Y'),
-            starttime=event.start.strftime('%H:%M'),
-            enddate=event.end.strftime('%d.%m.%Y'),
-            endtime=event.end.strftime('%H:%M'),
+            sortdate=startdt.strftime('%Y%m%d%H%M'),
+            startdate=startdt.strftime('%d.%m.%Y'),
+            starttime=startdt.strftime('%H:%M'),
+            enddate=enddt.strftime('%d.%m.%Y'),
+            endtime=enddt.strftime('%H:%M'),
             all_day=event.all_day))
         
     next_events = sorted(next_events, key=lambda ev: ev['sortdate'])
